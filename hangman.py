223e61cf64695e0de1f759a722ff4c3fe480e237
Chuck Belcher <cfbelcher@gmail.com>
2022-12-13 10:41:22 -0500

    Added while loop to allow user to continue to guess until all of the letters have been filled in

--- hangman.py
+++ hangman.py
@@ -2,8 +2,6 @@ import random
 # Step 1 Create Word List
 word_list = ["aardvark", "baboon", "camel"]
 
-
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
@@ -14,21 +12,16 @@ for space in range(0, len(chosen_word)):
     word_display.append('_')
 print(word_display)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input('What letter do you guess? ').lower()
+while word_display.count('_') > 0:
+    # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input('What letter do you guess? ').lower()
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for index in range(0, len(chosen_word)):
-    if guess == chosen_word[index]:
-        word_display[index] = guess
+    # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    for index in range(0, len(chosen_word)):
+        if guess == chosen_word[index]:
+            word_display[index] = guess
 
-# TODO -4: - Loop through each position in the chosen_word;
-# TODO - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-print(word_display)
+    print(word_display)
 
-#TODO-1: - Use a while loop to let the user guess again.
-# The loop should only stop once the user has guessed all the
-# letters in the chosen_word and 'display' has no more blanks ("_").
-# Then you can tell the user they've won.
+print('Congratulations, You Win !!!!')
+
