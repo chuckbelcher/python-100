dba99f05aaceb9686f7217817bd5bc971c323cda
Chuck Belcher <cfbelcher@pateogroup.com>
2023-11-13 20:10:49 -0500

    Removed duplicate code that was causing initial guess not to be rendered within the loop

--- hangman.py
+++ hangman.py
@@ -71,29 +71,20 @@ for space in range(0, word_length):
     word_display.append('_')
 print(word_display)
 
-#  Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input('What letter do you guess? ').lower()
-
-#  Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for index in range(0, word_length):
-    if guess == chosen_word[index]:
-        word_display[index] = guess
-
-print(word_display)
-
 while word_display.count('_') > 0 and lives > 0:
-    print(f"you have {lives} lives left")
     # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
     guess = input('What letter do you guess? ').lower()
     if guess not in chosen_word:
         lives -= 1
-
-    # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-    for index in range(0, len(chosen_word)):
-        if guess == chosen_word[index]:
-            word_display[index] = guess
+    else:
+        # Add guessed letter to word_display in correct spot
+        for index in range(0, word_length):
+            if guess == chosen_word[index]:
+                word_display[index] = guess
+    
     print(stages[lives])
     print(word_display)
+    print(f"you have {lives} lives left")
 
 if lives == 0:
     print("You Loose")
