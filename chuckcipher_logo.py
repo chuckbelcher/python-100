4d07f0a911e5ef2f731ba086d1c9f2e51d45324a
Chuck Belcher <cfbelcher@pateogroup.com>
2023-11-17 16:04:45 -0500

    Added link to ascii art generator used to create logo

--- chuckcipher_logo.py
+++ chuckcipher_logo.py
@@ -6,3 +6,4 @@ logo = """
   \____|_| |_|\__,_|\___|_|\_\  \____|_| .__/|_| |_|\___|_|   
                                        |_|                    
 """
+# ascii art generated here: https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=Red%20Phoenix&text=Your%20text%20here%20
