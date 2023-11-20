d65b9c37c02ecacbc85a3c580e61db6d468981c0
Chuck Belcher <cfbelcher@pateogroup.com>
2023-11-19 20:57:40 -0500

    Created a new function called cipher that replaced both encode and decode.

--- chuck_cipher.py
+++ chuck_cipher.py
@@ -4,21 +4,14 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 print(logo)
 
-def encrypt(text, shift):
-    print(f"Getting ready to encrypt {text} by {shift} positions")
+def cipher(text, shift, direction):
     processed_text = ""
     for letter in text:
         curr_pos = alphabet.index(letter)
-        cipher_pos = curr_pos + shift
-        processed_text += alphabet[cipher_pos]
-    return processed_text
-
-def decrypt(text, shift):
-    print(f'Getting ready to decrypt {text} by {shift} positions')
-    processed_text = ""
-    for letter in text:
-        curr_pos = alphabet.index(letter)
-        cipher_pos = curr_pos - shift
+        if direction == 'encode':
+            cipher_pos = curr_pos + shift
+        elif direction == 'decode':
+            cipher_pos = curr_pos - shift
         processed_text += alphabet[cipher_pos]
     return processed_text
 
@@ -26,5 +19,6 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-final_text = encrypt(text=text, shift=shift)
+final_text = cipher(text=text, shift=shift, direction=direction)
+
 print(f"The result from your request is {final_text}")
