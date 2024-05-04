78b64be5be043453543f307952b28f24ac87479e
Chuck Belcher <cfbelcher@pateogroup.com>
2024-05-03 22:16:11 -0400

    added inventory and make_coffee functions

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -1,11 +1,11 @@
 from data import cm_data
 
-#starting values
+# Starting Values
 water = 0
 milk = 0
 coffee = 0
 till = 0
-
+running = True
 
 def reset_inventory():
     water = cm_data.resources["water"]
@@ -21,13 +21,36 @@ def print_inventory():
     print(f"Coffee: {coffee}g")
     print(f"Money: ${till}")
 
-# TODO: Allow user to make a request
+
+def make_coffee(type):
+    print(f"Making {type} ...")
+
+reset_inventory()
+
+while running:
+    action = input("What would you like to do? (espresso/latte/cappuccino): ")
+    if action == "report":
+        print_inventory()
+    elif action == "espresso":
+        make_coffee("espresso")
+    elif action == "latte":
+        make_coffee("latte")
+    elif action == "cappuccino":
+        make_coffee("cappuccino")
+    elif action == "off":
+        print("Shutting down ...")
+        running = False
+    elif action == "reload":
+        reset_inventory()
+    else:
+        print("Invalid selection")
+
 # TODO: Check to see if there is enough inventory to fulfill request
 # TODO: Process Coins
 # TODO: Check to see if the transaction was successful
 # TODO: make the coffee
 # TODO: Update inventory
-# TODO: Reload Inventory
 
-reset_inventory()
-print_inventory()
+
+
+
