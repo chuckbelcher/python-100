47e2cea5577f4f5883352cc8b3ecd6f1eb8c7262
Chuck Belcher <cfbelcher@pateogroup.com>
2025-08-04 12:23:20 -0400

    added logic to check the user's response for accuracy and end the quiz when there is not another question.

--- D34/ui.py
+++ D34/ui.py
@@ -26,11 +26,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=self.true_image, bg="green", fg="white", command=lambda: print("True clicked"))
+        self.true_button = Button(image=self.true_image, bg="green", fg="white", command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
         self.false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=self.false_image, bg="red", fg="white", command=lambda: print("False clicked"))
+        self.false_button = Button(image=self.false_image, bg="red", fg="white", command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -39,8 +39,32 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
+        if not self.quiz.still_has_questions():
+            self.canvas.itemconfig(self.question_text, text="You've completed the quiz!")
+            self.canvas.config(bg="lightgrey")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+            return
         q_text = self.quiz.next_question()
         print(f"Q.{self.quiz.question_number}: {q_text}")
         self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)  #Adding the give_feedback method call here provides better readability.
+
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+        self.window.after(1000, self.get_next_question)
 
 
